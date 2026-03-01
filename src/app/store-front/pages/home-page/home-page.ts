33f04aca55920bb2535e0ee277a331d1ec43ee9c
jsubarte <jsubarte@users.noreply.github.com>
2026-02-28 21:59:25 -0500

    Completado hasta creacion de paths alias

---
+++ src/app/store-front/pages/home-page/home-page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { ProductCard } from '@products/components/product-card/product-card';
+//import { ProductCard } from "../../../products/components/product-card/product-card";
+
+@Component({
+  selector: 'app-home-page',
+  imports: [ ProductCard ],
+  templateUrl: './home-page.html',
+})
+export class HomePage { }
