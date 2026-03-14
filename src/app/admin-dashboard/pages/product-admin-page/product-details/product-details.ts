f3948f38b18adfdca23f2a423dc4738e7593d8da
jsubarte <jsubarte@users.noreply.github.com>
2026-03-14 16:03:44 -0500

    Formulario de productos iniciado

---
+++ src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.ts
@@ -0,0 +1,45 @@
+import { Component, inject, input, OnInit } from '@angular/core';
+import { IProduct } from '@products/interfaces/iproduct';
+import { ProductCarrousel } from "@products/components/product-carrousel/product-carrousel";
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '@utils/form-utils';
+
+@Component({
+  selector: 'product-details',
+  imports: [ProductCarrousel, ReactiveFormsModule],
+  templateUrl: './product-details.html',
+})
+export class ProductDetails implements OnInit {
+  product = input.required<IProduct>()
+
+  fb = inject(FormBuilder)
+
+  productForm = this.fb.group({
+    title: [ '', Validators.required ],
+    description: [ '', Validators.required ],
+    slug: [ '',
+      [ Validators.required, Validators.pattern(FormUtils.slugPattern )]
+    ],
+    price: [ 0, [ Validators.required, Validators.min(0) ] ],
+    stock: [ 0, [ Validators.required, Validators.min(0) ] ],
+    sizes: [['']],
+    images: [[]],
+    tags: [''],
+    gender: [ 'men', [ Validators.required, Validators.pattern(/men|women|kid|unisex/) ] ]
+  })
+
+  sizes = [ 'XS', 'S', 'M', 'L', 'XL', 'XXL' ]
+
+  ngOnInit(): void {
+    this.setFormValue(this.product())
+  }
+
+  setFormValue( formLike: Partial<IProduct> ) {
+    this.productForm.reset(this.product() as any)
+    this.productForm.patchValue({ tags: formLike.tags?.join(',') })
+  }
+
+  onSubmit() {
+    console.log(this.productForm.value)
+  }
+}
