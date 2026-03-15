f19d186ecdd410ccc93a2d318d49bb2762ea4244
jsubarte <jsubarte@users.noreply.github.com>
2026-03-15 13:29:55 -0500

    Paginación funcional completa

---
+++ src/app/shared/components/pagination/pagination.ts
@@ -0,0 +1,21 @@
+import { Component, computed, input, linkedSignal } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'pagination',
+  imports: [RouterLink],
+  templateUrl: './pagination.html',
+})
+export class Pagination {
+  pages = input<number>(0)
+
+  currentPage = input<number>(1)
+
+  activePage = linkedSignal(this.currentPage)
+
+  getPageList = computed(
+    () => {
+      return Array.from( { length: this.pages() }, ( _, i ) => i + 1 )
+    }
+  )
+}
--- src/app/store-front/pages/home-page/home-page.ts
+++ src/app/store-front/pages/home-page/home-page.ts
@@ -1,22 +1,39 @@
 import { Component, inject } from '@angular/core';
 import { ProductCard } from '@products/components/product-card/product-card';
 import { ProductsService } from '../../../products/services/products-service';
-import { rxResource } from '@angular/core/rxjs-interop';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { Pagination } from "@shared/components/pagination/pagination";
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
 
 
 
 @Component({
   selector: 'app-home-page',
-  imports: [ ProductCard ],
+  imports: [ProductCard, Pagination],
   templateUrl: './home-page.html',
 })
 export class HomePage {
   productsService = inject(ProductsService)
 
+  activatedRoute = inject(ActivatedRoute)
+
+  currentPage = toSignal(
+    this.activatedRoute.queryParamMap.pipe(
+      map( params => ( params.get('page') ? +params.get('page')! : 1 ) ),
+      map( page => ( isNaN(page) ? 1 : page ) )
+    ),
+    {
+      initialValue: 1
+    }
+  )
+
   productResource = rxResource({
-    params: () => ({}),
-    stream: ( { /* params */ }) => {
-      return this.productsService.getProducts({})
+    params: () => ({ page: this.currentPage() - 1 }),
+    stream: ( { params }) => {
+      return this.productsService.getProducts({
+        offset: params.page * 9
+      })
     }
   })
 
