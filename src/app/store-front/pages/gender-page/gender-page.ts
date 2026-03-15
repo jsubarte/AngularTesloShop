7d0968775ac6e7a3da79b0fd5bfd485f572e2319
jsubarte <jsubarte@users.noreply.github.com>
2026-03-15 14:32:18 -0500

    Paginacion completada

--- src/app/store-front/pages/gender-page/gender-page.ts
+++ src/app/store-front/pages/gender-page/gender-page.ts
@@ -4,14 +4,18 @@ import { ActivatedRoute } from '@angular/router';
 import { ProductsService } from '@products/services/products-service';
 import { map } from 'rxjs';
 import { ProductCard } from "@products/components/product-card/product-card";
+import { Pagination } from "@shared/components/pagination/pagination";
+import { PaginationService } from '@shared/components/pagination/pagination-service';
 
 @Component({
   selector: 'app-gender-page',
-  imports: [ProductCard],
+  imports: [ProductCard, Pagination],
   templateUrl: './gender-page.html',
 })
 export class GenderPage {
   route = inject(ActivatedRoute)
+  productsService = inject(ProductsService)
+  paginationService = inject(PaginationService)
 
   gender = toSignal(
     this.route.params.pipe(
@@ -19,13 +23,13 @@ export class GenderPage {
     )
   )
 
-  productsService = inject(ProductsService)
 
   productResource = rxResource({
-    params: () => ({ gender: this.gender() }),
+    params: () => ({ gender: this.gender(), page: this.paginationService.currentPage() - 1 }),
     stream: ( { params }) => {
       return this.productsService.getProducts({
-        gender: params.gender
+        gender: params.gender,
+        offset: params.page * 9
       })
     }
   })
--- src/app/store-front/pages/home-page/home-page.ts
+++ src/app/store-front/pages/home-page/home-page.ts
@@ -1,10 +1,9 @@
 import { Component, inject } from '@angular/core';
 import { ProductCard } from '@products/components/product-card/product-card';
 import { ProductsService } from '../../../products/services/products-service';
-import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { rxResource } from '@angular/core/rxjs-interop';
 import { Pagination } from "@shared/components/pagination/pagination";
-import { ActivatedRoute } from '@angular/router';
-import { map } from 'rxjs';
+import { PaginationService } from '@shared/components/pagination/pagination-service';
 
 
 
@@ -16,20 +15,10 @@ import { map } from 'rxjs';
 export class HomePage {
   productsService = inject(ProductsService)
 
-  activatedRoute = inject(ActivatedRoute)
-
-  currentPage = toSignal(
-    this.activatedRoute.queryParamMap.pipe(
-      map( params => ( params.get('page') ? +params.get('page')! : 1 ) ),
-      map( page => ( isNaN(page) ? 1 : page ) )
-    ),
-    {
-      initialValue: 1
-    }
-  )
+  paginationService = inject(PaginationService)
 
   productResource = rxResource({
-    params: () => ({ page: this.currentPage() - 1 }),
+    params: () => ({ page: this.paginationService.currentPage() - 1 }),
     stream: ( { params }) => {
       return this.productsService.getProducts({
         offset: params.page * 9
